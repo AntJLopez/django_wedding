17f09c6a3e30b2aad17ec9c7a0f1b2d31b3c9ff5
Antonio J. Lopez <antonio@lopez.io>
2018-08-08 10:45:13 +0000

    Configure site for production

--- wedding/settings.py
+++ wedding/settings.py
@@ -1,5 +1,5 @@
 import os
-from .local_settings import DATABASES, SECRET_KEY
+from .local_settings import DATABASES, SECRET_KEY, ALLOWED_HOSTS
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -7,8 +7,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
-
-ALLOWED_HOSTS = []
 
 
 # Application definition
