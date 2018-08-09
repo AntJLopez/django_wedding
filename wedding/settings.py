33096da64c6eb4add675494542b7029a9db71e9b
Antonio J. López <antonio@lopez.io>
2018-08-09 06:27:49 -0400

    Make DEBUG a local setting

--- wedding/settings.py
+++ wedding/settings.py
@@ -1,16 +1,11 @@
 import os
-from .local_settings import DATABASES, SECRET_KEY, ALLOWED_HOSTS
+from .local_settings import DATABASES, SECRET_KEY, ALLOWED_HOSTS, DEBUG
+
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
-
-# SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
-
-
 # Application definition
-
 INSTALLED_APPS = [
     'django.contrib.admin',
     'django.contrib.auth',
