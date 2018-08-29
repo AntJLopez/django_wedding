5362d1509ac4d910c42978d778bdea7399c27322
Antonio J. López <antonio@lopez.io>
2018-08-29 01:51:15 -0400

    Remove qa for imports

--- wedding/views.py
+++ wedding/views.py
@@ -1,6 +1,7 @@
-from django.shortcuts import render, redirect, get_object_or_404
-from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, redirect, get_object_or_404  # noqa
+from django.contrib.auth.decorators import login_required  # noqa
 from .local_settings import GOOGLE_API_KEY
+
 
 def home(request):
     params = {'google_api_key': GOOGLE_API_KEY}
