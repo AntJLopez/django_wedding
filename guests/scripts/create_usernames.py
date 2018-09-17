f3ff6d09abd0159bbf66f5bf3dfcaee9270fd4e5
Antonio J. López <antonio@lopez.io>
2018-09-17 19:58:19 -0400

    Undo fix which didn't work

--- guests/scripts/create_usernames.py
+++ guests/scripts/create_usernames.py
@@ -8,7 +8,6 @@ def run(*args):
         if (guest.username and len(guest.username) == 10 and
                 guest.username.isupper()):
             guest.username = None
-            guest.save()
         if all(not v for v in [
                 guest.username, guest.lead_partner, guest.parent]):
             # Guest needs a username and has none
