6c855fa0d62a1fc2c9909148040c5db3852b3f84
Antonio J. López <antonio@lopez.io>
2018-10-02 17:42:40 -0400

    Format RSVP form

--- guests/views.py
+++ guests/views.py
@@ -1,7 +1,13 @@
+from django.http import JsonResponse
+from django.views.decorators.http import require_POST
+from wedding.local_settings import STRIPE_SECRET_KEY
+import stripe
 from django.shortcuts import render, redirect, get_object_or_404
 from django.contrib.auth.decorators import login_required
 from .models import Guest
-from .forms import GuestForm
+from .forms import GuestForm, RSVPForm
+
+stripe.api_key = STRIPE_SECRET_KEY
 
 
 @login_required
@@ -47,3 +53,17 @@ def guest_delete(request, pk, template_name='guests/delete.html'):
         return redirect('guest_list')
     data = {'guest': guest}
     return render(request, template_name, data)
+
+
+@require_POST
+def rsvp(request):
+    response = {'errors': {}}
+
+    form = RSVPForm(request.POST)
+    if form.is_valid():
+        cd = form.cleaned_data
+    else:
+        # The form is invalid
+        response['errors'] = form.errors
+
+    return JsonResponse(response)
